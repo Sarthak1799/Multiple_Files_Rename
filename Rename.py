c2ea5cbd5e8376f37ea2c05f0f4f31f0eb865f3c
Sarthak <sarthakasoni@gmail.com>
2020-12-22 21:10:56 +0530

    modified

--- Rename.py
+++ Rename.py
@@ -5,27 +5,27 @@ filenumber = 1
 
 if os.path.isdir(folderpath):
     
-    a = input("looking for a specific format? ")
-    if(a=='yes'):
+    a = input("looking for a specific format? [y/n] ")
+    if(a=='y'):
         f = input("What format are you looking for? ")
 
 
-    ask = input("Should I apply format? ")
-    if(ask=='yes'):
-        format = input("What format Should I apply?")
-    elif(ask=='no'):
+    ask = input("Should I apply format? [y/n] ")
+    if(ask=='y'):
+        format = input("What format Should I apply? (ex - .png) ")
+    elif(ask=='n'):
         print("OK, moving on...")
     label = input("Enter the new label ")
 
     for filename in os.listdir(folderpath):
         name,extension = os.path.splitext(filename)
-        if(a=='yes' and f==extension):
-            if(ask=='yes'):
+        if(a=='y' and f==extension):
+            if(ask=='y'):
              os.rename(folderpath + '\\' + filename, folderpath + '\\' + label + str(filenumber) + format)
             else:
              os.rename(folderpath + '\\' + filename, folderpath + '\\' + label + str(filenumber) + extension)
         else:
-            if(ask=='yes'):
+            if(ask=='y'):
              os.rename(folderpath + '\\' + filename, folderpath + '\\' + label + str(filenumber) + format)
             else:
              os.rename(folderpath + '\\' + filename, folderpath + '\\' + label + str(filenumber) + extension)
